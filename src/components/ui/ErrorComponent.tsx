518e7dbbb79b3278c0ab0037f5cbf970edde85d3
nirjhorsaha <nirjhor.badhan25@gmail.com>
2024-09-03 20:29:19 +0600

    [Modified] error components

--- src/components/ui/ErrorComponent.tsx
+++ src/components/ui/ErrorComponent.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaExclamationTriangle } from 'react-icons/fa'; // Using FontAwesome for the error icon
+import { FaExclamationTriangle } from 'react-icons/fa';
 
 interface ErrorComponentProps {
   message: string;
@@ -10,10 +10,10 @@ const ErrorComponent: React.FC<ErrorComponentProps> = ({ message }) => {
     <div className="flex justify-center items-center h-screen">
       <div className="text-center space-y-4">
         <div className="flex justify-center">
-          <FaExclamationTriangle className="text-red-600 text-6xl" /> {/* Error icon */}
+          <FaExclamationTriangle className="text-red-600 text-6xl" />
         </div>
-        <p className="text-xl text-red-600 font-semibold mb-2">Error</p>
-        <p className="text-gray-600">{message}</p>
+        <p className="text-xl text-red-600 font-semibold mb-2"></p>
+        <p className="text-gray-600 text-xl font-semibold">{message}</p>
       </div>
     </div>
   );
