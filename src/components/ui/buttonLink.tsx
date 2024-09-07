8af08b3c95010b741b27c242270a7e4f391ba1fa
nirjhorsaha <nirjhor.badhan25@gmail.com>
2024-09-07 20:12:06 +0600

    remove extra code

--- src/components/ui/buttonLink.tsx
+++ src/components/ui/buttonLink.tsx
@@ -4,42 +4,20 @@ import { Link } from 'react-router-dom';
 interface ButtonLinkProps {
   to?: string;
   text: string;
-  onClick?: () => void; // Added onClick prop for handling custom actions
-  className?: string; // Optional className prop for additional styling
+  className?: string; 
+  onClick?: () => void; 
 }
 
-const ButtonLink: React.FC<ButtonLinkProps> = ({ to, text, onClick, className = '' }) => {
+const ButtonLink: React.FC<ButtonLinkProps> = ({ to = '', text, onClick, className = '' }) => {
   return (
-    <button
+    <Link
       onClick={onClick}
+      to={to}
       className={`bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 text-white px-6 py-3 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 ${className}`}
     >
-      {to ? <Link to={to}>{text}</Link> : text}
-    </button>
+      {text}
+    </Link>
   );
 };
 
 export default ButtonLink;
-
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-
-// interface ButtonLinkProps {
-//   to: string;
-//   text: string;
-//   className?: string; // Optional className prop for additional styling
-//   onClick?: () => void; // Added onClick prop for handling custom actions
-// }
-
-// const ButtonLink: React.FC<ButtonLinkProps> = ({ to, text, onClick, className = '' }) => {
-//   return (
-//     <Link
-//       to={to}
-//       className={`bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 text-white px-6 py-3 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 ${className}`}
-//     >
-//       {text}
-//     </Link>
-//   );
-// };
-
-// export default ButtonLink;
